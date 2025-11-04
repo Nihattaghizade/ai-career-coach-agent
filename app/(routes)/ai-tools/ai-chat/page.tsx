ed8e62e4a8012deada4f42e5b965561647abca8d
Nihattaghizade <nihattagizade2003@gmail.com>
2025-11-05 00:23:51 +0400

    add ai chat ui

--- app/(routes)/ai-tools/ai-chat/page.tsx
+++ app/(routes)/ai-tools/ai-chat/page.tsx
@@ -1,10 +1,12 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Send } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import EmptyState from "./_components/EmptyState";
 
 function AiChat() {
+  const [userInput, setUserInput] = useState<string>();
   return (
     <div className="px-10 md:px-24 lg:px-36 xl:px-48">
       <div className="flex items-center justify-between gap-8">
@@ -12,25 +14,34 @@ function AiChat() {
           <h2 className="font-bold text-lg">AI Career Q/A Chat</h2>
           <p>
             Smarter career decisions start here - get tailored advice, real-time
-            market insights, and a roadmap built just for you with the power of
-            AI.
+            market insights
           </p>
         </div>
         <Button>+ New Chat</Button>
       </div>
 
       <div className="flex flex-col h-[75vh]">
-        <div>{/* Empty State Options */}
-          <EmptyState />
+        <div className="mt-5">
+          {/* Empty State Options */}
+          <EmptyState
+            selectedQuestion={(question: string) => setUserInput(question)}
+          />
         </div>
 
         <div className="flex-1">{/* Message List */}</div>
 
         <div className="flex justify-between items-center gap-6">
           {/* Input Field */}
-          <Input placeholder="Type here" />
-          <Button> <Send /> </Button>
-          </div>
+          <Input
+            placeholder="Type here"
+            value={userInput}
+            onChange={(event) => setUserInput(event.target.value)}
+          />
+          <Button>
+            {" "}
+            <Send />{" "}
+          </Button>
+        </div>
       </div>
     </div>
   );
